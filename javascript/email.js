e217508764d970a014ab5516285f9d448a55a449
Vinzent786 <vincentcook6864@gmail.com>
2023-11-09 12:25:19 -0500

    first commit

---
+++ javascript/email.js
@@ -0,0 +1,69 @@
+const c = (x) => {console.log(x)}
+
+function submit() {
+    let name, email, message;
+    const inputList = Array.from($('input, textarea'));
+    let readyToSend = true;
+    inputList.forEach(element => {
+            if (element.value.length > 0) {
+                switch ($(element).attr('name')) {
+                    case 'name':
+                        name = element.value;
+                        break;
+                    case 'email':
+                        email = element.value;
+                        break;
+                    case 'message':
+                        message = element.value;
+                        break;
+                    default:
+                        break;
+                }
+        } else {
+            readyToSend = false;
+            $(element).addClass('err');
+            setTimeout(()=> $(element).removeClass('err'), 1000);
+            switch ($(element).attr('name')) {
+                case 'name':
+                    $(element).attr('placeholder', 'Must enter your Name');
+                    break;
+                case 'email':
+                    $(element).attr('placeholder', 'Must enter your Email');
+                    break;
+                case 'message':
+                    $(element).attr('placeholder', 'Must enter a Message');
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+    c(recaptchaCompleted)
+    if (readyToSend && recaptchaCompleted) {
+        emailjs.send('service_8pl1o1h', 'template_8ianm4o', {
+            name: name,
+            email: email,
+            message: message
+        })
+        .then((response, err) => {
+            if(response){
+                $('section').css('display', 'none');
+                $('address').css('display', 'none');
+                $('dialog').attr('open', 'true');
+                $('dialog').text(`Message sent successfully! You will hear back from us soon at ${email}`);
+            } else {
+                $('dialog').attr('open', 'true');
+                $('dialog').text(`There was an error processesing your message. Error: ${err}`);
+            }
+        });
+    }
+}
+
+$(document).ready(() => {
+    $('form').submit((event) => {
+        event.preventDefault();
+        submit();
+    });
+    emailjs.init('Qmj-x8EzmOhgBtNF5');
+});
+
---
+++ javascript/gallery.js
@@ -0,0 +1,50 @@
+const c = (x) => console.log(x);
+
+function scroll(direction) {
+    const imgsArr = ['/assets/images/gallery1.webp', '/assets/images/gallery2.webp', '/assets/images/gallery3.webp', '/assets/images/gallery4.webp', '/assets/images/gallery5.webp', '/assets/images/gallery6.webp', '/assets/images/gallery7.webp'];
+    let currentImgs = Array.from($('#gallery').find('> div > img'));
+
+    switch (direction) {
+        case 'left':
+            for (let i = 0; i < currentImgs.length; i++) {
+                if (i === 0) {
+                    currentImgs[i].src = imgsArr[imgsArr.length - 1];
+                } else {
+                    currentImgs[i].src = imgsArr[i - 1];
+                }
+            }
+            break;
+        case 'right':
+            for (let i = 0; i < currentImgs.length; i++) {
+                currentImgs[i].src = imgsArr[(i + 1) % imgsArr.length];
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+
+function adjustGallery(gallery) {
+    if ($(window).width() > 1600) {
+        if ($(gallery).find('#newDiv').length > 0) return;
+        const newDiv = $('<div></div>');
+        const newImg = $('<img />');
+        newImg.attr('src', '/assets/images/gallery4.webp');
+        newImg.attr('alt', 'Room Image');
+        newImg.attr('loading', 'lazy');
+        newDiv.append(newImg);
+        newDiv.attr('id', 'newDiv');
+        gallery.append(newDiv);
+    } else if ($(window).width() <= 1600) {
+        if (!$(gallery).find('#newDiv').length > 0) return;
+        $('#newDiv').remove();
+    }
+}
+
+$(document).ready(() => {
+    adjustGallery($('#gallery'));
+    $(window).on('resize', () => adjustGallery($('#gallery')));
+    $('#left').on('click', () => scroll('left'));
+    $('#right').on('click', () => scroll('right'));
+});

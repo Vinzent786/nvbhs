6af49c3faba9ba8a2666e6625b9c3c9e4b0b0a5c
Vinzent786 <vincentcook6864@gmail.com>
2023-11-16 14:26:04 -0500

    Finilizing site

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,21 +1,20 @@
 const c = (x) => console.log(x);
 
 function scroll(direction) {
-    let currentImgs = Array.from($('#gallery').find('> div > img'));
-
+    let currentImgs = Array.from($('#gallery').find('> div > img')); //Array current children of #gallery
     switch (direction) {
         case 'left':
             for (let i = 0; i < currentImgs.length; i++) {
-                let srcNum = parseInt(currentImgs[i].src.split('gallery')[1].split('.')[0]);
+                let srcNum = parseInt(currentImgs[i].src.split('gallery')[1].split('.')[0]); //number after 'gallery' in src
                 if (srcNum === 6) {
-                    let src = `/nvbhs/assets/images/gallery0.webp`;
+                    let src = `/nvbhs/assets/images/gallery0.webp`; //Loops back to first image
                     currentImgs[i].src = src;
                 } else {
-                    let src = `/nvbhs/assets/images/gallery${srcNum + 1}.webp`;
+                    let src = `/nvbhs/assets/images/gallery${srcNum + 1}.webp`; //Changes src to next image to be displayed
                     currentImgs[i].src = src;
                 }
                 $(currentImgs[i]).addClass('fadeIn');
-                setTimeout(() => $(currentImgs[i]).removeClass('fadeIn', 5000));
+                setTimeout(() => $(currentImgs[i]).removeClass('fadeIn'), 200);
             }
             break;
         case 'right':
@@ -28,6 +27,8 @@ function scroll(direction) {
                     let src = `/nvbhs/assets/images/gallery${srcNum - 1}.webp`;
                     currentImgs[i].src = src;
                 }
+                $(currentImgs[i]).addClass('fadeIn');
+                setTimeout(() => $(currentImgs[i]).removeClass('fadeIn'), 200);
             }
             break;
         default:
@@ -35,21 +36,52 @@ function scroll(direction) {
     }
 }
 
-
 function adjustGallery(gallery) {
-    if ($(window).width() > 1600) {
-        if ($(gallery).find('#newDiv').length > 0) return;
+    let currentImgs = Array.from($('#gallery').find('> div > img')); //Gets array of current children of #gallery
+    if ($(window).width() > 1600) { //This makes sure that there are 4 images displayed at once on this screen size
+        if (currentImgs.length === 4) return;
         const newDiv = $('<div></div>');
         const newImg = $('<img />');
-        newImg.attr('src', 'assets/images/gallery4.webp');
+        let srcNum = parseInt($('#gallery div:last > img')[0].src.split('gallery')[1].split('.')[0]);
+        let num;
+        if (srcNum === 6) {
+            num = 0;
+        } else {
+            num = srcNum + 1;
+        }
+        newImg.attr('src', `assets/images/gallery${num}.webp`);
         newImg.attr('alt', 'Room Image');
         newImg.attr('loading', 'lazy');
         newDiv.append(newImg);
-        newDiv.attr('id', 'newDiv');
         gallery.append(newDiv);
-    } else if ($(window).width() <= 1600) {
-        if (!$(gallery).find('#newDiv').length > 0) return;
-        $('#newDiv').remove();
+    } else if ($(window).width() > 600 && $(window).width() < 1600) { //This makes sure that there are three images displayed at once on this screen size
+        if (currentImgs.length === 4) {
+            c('removing')
+            $('#gallery div:last').remove();
+        } else if (currentImgs.length === 3) {
+            return;
+        } else if (currentImgs.length === 2) {
+            const newDiv = $('<div></div>');
+            const newImg = $('<img />');
+            let srcNum = parseInt($('#gallery div:last > img')[0].src.split('gallery')[1].split('.')[0]);
+            let num;
+            if (srcNum === 6) {
+                num = 0;
+            } else {
+                num = srcNum + 1;
+            }
+            newImg.attr('src', `assets/images/gallery${num}.webp`);
+            newImg.attr('alt', 'Room Image');
+            newImg.attr('loading', 'lazy');
+            newDiv.append(newImg);
+            gallery.append(newDiv);
+        }
+    } else if ($(window).width() < 600) { //This makes sure that there are two images displayed at once on this screen size
+        if (currentImgs.length === 2) {
+            return;
+        } else if (currentImgs.length > 2) {
+            $('#gallery div:last').remove();
+        }
     }
 }
 
@@ -59,20 +91,3 @@ $(document).ready(() => {
     $('#left').on('click', () => scroll('left'));
     $('#right').on('click', () => scroll('right'));
 });
-
-
-
-// const imgsArr = ['assets/images/gallery0.webp', 'assets/images/gallery1.webp', 'assets/images/gallery2.webp', 'assets/images/gallery3.webp', 'assets/images/gallery4.webp', 'assets/images/gallery5.webp', 'assets/images/gallery6.webp'];
-// let currentImgs = Array.from($('#gallery').find('> div > img'));
-
-// switch (direction) {
-//     case 'left':
-//         for (let i = 0; i < currentImgs.length; i++) {
-//             c(i)
-//             c(`current src - ${currentImgs[i].src}\nchanging src to - ${imgsArr[i + 1]}`);
-//             currentImgs[i].src = imgsArr[i + 1];
-//         }
-//         break;
-//     default:
-//         break;
-// }
